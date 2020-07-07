a1c90451bf70726ef8ee27502464317b4697f2e9
Philipp <P.Hellwich@gmx.de>
2020-07-07 16:53:00 +0200

    initial

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
-import store from './store'
-
+import Vue from "vue"
+import App from "./App.vue"
+import store from "./store"
+import drizzleVuePlugin from "@drizzle/vue-plugin"
+import drizzleOptions from "./drizzleOptions"
 Vue.config.productionTip = false
-
+Vue.use(drizzleVuePlugin, { store, drizzleOptions })
 new Vue({
   store,
-  render: function (h) { return h(App) }
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app")
